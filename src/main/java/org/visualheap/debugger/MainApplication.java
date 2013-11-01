ebcfa7ef78e4938c415d9f0e8de2eb8356062e3c
Aviv Beeri <avbeeri@gmail.com>
2013-11-01 11:58:51 +0000

    You can now set the class path via the dialog and type in the package/class name

--- src/main/java/org/visualheap/debugger/MainApplication.java
+++ src/main/java/org/visualheap/debugger/MainApplication.java
@@ -11,26 +11,34 @@ import java.io.*;
 
 public class MainApplication {
 
-  private static final JTextArea taConsoleOutput = new JTextArea();
+  //Variables
+	private static Debugger debugger;
+  private static String classPath;
+	private static String className;
+  
+	//GUI Components
+	private static final JTextArea taConsoleOutput = new JTextArea();
   private static final JTextArea taDebuggerOutput = new JTextArea();
   private static final JFileChooser fc = new JFileChooser();
-  private static Debugger debugger;
-
+  
 
   private static void buildGUI() {
 
+    //Generate the GUI root frame
 		JFrame frame = new JFrame("Visual Heap Analyser");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
+		//Base pane to add content to
 		JPanel contentPane = new JPanel(new BorderLayout());
 		frame.setContentPane(contentPane);
 
+    //Creates the pane containing the console and debugger outputs
 		JPanel consolePane = new JPanel(new GridBagLayout());
 		GridBagConstraints constraints = new GridBagConstraints();
 		contentPane.add(consolePane,BorderLayout.CENTER);
 
 		JScrollPane debuggerScrollPane = new JScrollPane(taDebuggerOutput);
-		debuggerScrollPane.setPreferredSize(new Dimension(200, 180));
+		debuggerScrollPane.setPreferredSize(new Dimension(300, 380));
 		constraints.fill = GridBagConstraints.BOTH;
 		constraints.weightx=0.5;
 		constraints.weighty=0.5;
@@ -38,10 +46,13 @@ public class MainApplication {
 		constraints.gridx=0;
     consolePane.add(debuggerScrollPane, constraints);
 
+    //This thread sets up the MainApplication's output and 
+		//error streams to the taDebuggerOutput.
 
 		Thread d = new Thread() {
 			public void run() {
 				try {
+					System.setErr(System.out);
 					PipedOutputStream pOut = new PipedOutputStream();
 					System.setOut(new PrintStream(pOut));
 					PipedInputStream pIn = new PipedInputStream(pOut);
@@ -56,30 +67,42 @@ public class MainApplication {
 		};
 		d.start();
 		
+		
 		JScrollPane consoleScrollPane = new JScrollPane(taConsoleOutput);
-		consoleScrollPane.setPreferredSize(new Dimension(200, 180));
+		consoleScrollPane.setPreferredSize(new Dimension(300, 380));
 		constraints.gridx=1;
     consolePane.add(consoleScrollPane, constraints);
 
-    JButton button = new JButton("Choose class");
+    JPanel fileSelectPane = new JPanel();
+		fileSelectPane.setLayout(new BoxLayout(fileSelectPane, BoxLayout.Y_AXIS));
+    frame.getContentPane().add(fileSelectPane, BorderLayout.NORTH);
+    
+
+    JButton button = new JButton("Choose classpath");
 		button.setPreferredSize(new Dimension(150, 40));
 		button.addActionListener(new ActionListener() {
  			public void actionPerformed(ActionEvent e) {
 				//In response to a button click:
-				
+			  fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 				int returnVal = fc.showOpenDialog(null);
 			  if (returnVal == JFileChooser.APPROVE_OPTION) {
-          taConsoleOutput.append("File found: " +   fc.getSelectedFile().getPath() + "\n");
+          taConsoleOutput.append("Classpath found: " +   fc.getSelectedFile().getPath() + "\n");
+				  classPath = fc.getSelectedFile().getPath();
 				}
 			}
 		});
-    frame.getContentPane().add(button, BorderLayout.PAGE_START);
+    fileSelectPane.add(button);
 
-    JButton btnDebug = new JButton("Debug");
+    final JTextField edtClassName = new JTextField();
+		fileSelectPane.add(edtClassName);
+    
+		
+		JButton btnDebug = new JButton("Debug");
 		btnDebug.setPreferredSize(new Dimension(150, 40));
 		btnDebug.addActionListener(new ActionListener() {
  			public void actionPerformed(ActionEvent e) {
-				  debugger = new Debugger("build/classes/test", "debugger.testprogs.Array", 12, new TestDebugListener() {
+				  className = edtClassName.getText();
+				  debugger = new Debugger(classPath, className, 12, new TestDebugListener() {
 					  public void onBreakpoint(List<ObjectReference> ref) {
 						  super.onBreakpoint(ref);
 							System.out.println("BREAKPOINT, resuming....");
