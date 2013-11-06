f22bbc9e454c302c964fd7f456e30301af732032
Anna <at711@ic.ac.uk>
2013-11-06 12:53:08 +0000

    HeapListener gets inbound and outbound references and detects cyclic references.

--- src/main/java/org/visualheap/app/HeapListener.java
+++ src/main/java/org/visualheap/app/HeapListener.java
@@ -6,6 +6,7 @@ import org.visualheap.debugger.NullListener;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,9 +19,7 @@ public class HeapListener extends NullListener {
     @Override
     public void newOnBreakpoint(StackFrame sf) {
         System.out.println("HeapListener breakpoint, got object references");
-        //if the PrintWriter points to System.out, which can happen if the writer fails,
-        //make sure we don't close it.
-        boolean close = true;
+
         PrintWriter writer = null;
         try {
             writer = new PrintWriter("out/HeapListenerOutput.txt", "UTF-8");
@@ -28,15 +27,7 @@ public class HeapListener extends NullListener {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
-        } finally {
-        	
-        	if (writer == null) {	
-            	writer = new PrintWriter(System.out);
-            	close = false;
-            }
         }
-        
-        
         writer.println("-------- Heap Listener Output --------");
         writer.println("");
 
@@ -44,18 +35,15 @@ public class HeapListener extends NullListener {
             for(LocalVariable lv : sf.visibleVariables()) {
                 Value val = sf.getValue(lv);
                 if(val instanceof ObjectReference) {
-                    // Object reference
-                    writer.println("ObjectReference");
-                    writer.println("    ID:         " + val.hashCode());
-                    writer.println("    TypeName:   " + lv.typeName());
-                    writer.println("    Val:        " + val.toString());
-                    //System.out.println("ObjectReference found: " + lv.typeName() + " " + val.toString());
+                    getReferences(((ObjectReference) val), writer, new HashMap<Integer, ObjectReference>());
                 } else {
                     writer.println("LocalVariable");
-                    writer.println("    ID:         " + val.hashCode());
+                    writer.println("    HashCode:   " + val.hashCode());
+                    //writer.println("    UniqueId:   " + ((ObjectReference) val).uniqueID());
                     writer.println("    TypeName:   " + lv.typeName());
                     writer.println("    Val:        " + val.toString());
-                    //System.out.println("LocalVariable found: " + lv.typeName() + " "  + val.toString());
+                    writer.println("    IsArgument: " + lv.isArgument());
+                    writer.println("    IsVisible:  " + lv.isVisible(sf));
                 }
             }
         } catch (AbsentInformationException e) {
@@ -63,10 +51,38 @@ public class HeapListener extends NullListener {
             // this might happen
             e.printStackTrace();
         }
-        if (close) {
-        	writer.close();
-        } else {
-        	writer.flush();
+        writer.close();
+    }
+
+    public void getReferences(ObjectReference val, PrintWriter writer, HashMap<Integer, ObjectReference> prevRefs) {
+        if (prevRefs.containsKey(val.hashCode())) {
+            writer.println("Cyclic reference found");
+            return;
+        }
+        ReferenceType objectType = val.referenceType();
+
+        writer.println("ObjectReference");
+        writer.println("    HashCode:   " + val.hashCode());
+        //writer.println("    UniqueId:   " + ((ObjectReference) val).uniqueID());
+        writer.println("    TypeName:   " + objectType.name());
+        writer.println("    Val:        " + val.toString());
+        //writer.println("    IsArgument: " + lv.isArgument());
+        //writer.println("    IsVisible:  " + lv.isVisible(sf));
+
+        writer.println("");
+        // referringObjects(0) - Returns all objects that directly reference this object.
+        writer.println("    References: " + val.referringObjects(0));
+        List<Field> fields = objectType.allFields();
+
+        prevRefs.put(val.hashCode(), val);
+
+        for (Field f : fields) {
+            writer.println("    Values: ");
+            Value v = val.getValue(f);
+            if (v instanceof ObjectReference) {
+                writer.println("        " + v.toString());
+                getReferences(((ObjectReference) v), writer, prevRefs);
+            }
         }
     }
 
@@ -147,8 +163,9 @@ public class HeapListener extends NullListener {
 
 	@Override
 	public void onStep(List<ObjectReference> fromStackFrame) {
-		
-		System.out.println("HeapListener: onStep");
+		// TODO Auto-generated method stub
+        System.out.println("Stepped");
+
 	}
 
     @Override
