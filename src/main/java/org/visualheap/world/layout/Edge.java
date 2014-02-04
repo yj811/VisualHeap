314347a29d3249947f54ccdfbdc9f3cedb757d05
anna <annat21@live.co.uk>
2014-02-04 17:33:19 +0000

    Began adding base of cones. Maths needs fixing for rotation.

--- src/main/java/org/visualheap/world/layout/Edge.java
+++ src/main/java/org/visualheap/world/layout/Edge.java
@@ -5,15 +5,13 @@ import java.awt.geom.Point2D;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import com.jme3.material.Material;
-import com.jme3.math.Matrix3f;
 import com.jme3.math.Quaternion;
+import com.jme3.scene.shape.Cylinder;
 import org.visualheap.app.Game;
 
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Line;
-import com.jme3.scene.shape.Sphere;
 import com.jme3.scene.shape.Dome;
 import com.jme3.math.FastMath;
 
@@ -24,16 +22,15 @@ import com.jme3.math.FastMath;
  *
  */
 public class Edge implements ChangeListener {
-	
-	private static final int SPHERE_DISTANCE_FROM_ENDPOINT = 5;
-    private static final int SPHERE_RADIUS = 3;
     protected Vertex start;
 	protected Vertex end;
-    private Line line;
     private LayoutBuilder lb;
     private Geometry lineGeo;
-    private Geometry ballGeo;
-    private Dome ball;
+    private Geometry coneGeo;
+    private Geometry diskGeo;
+    private Line line;
+    private Dome cone;
+    private Cylinder disk;
 
 	public Edge(LayoutBuilder lb, Vertex start, Vertex end) {
 	    this.lb = lb;
@@ -64,12 +61,15 @@ public class Edge implements ChangeListener {
 		game.addNonCollidable(lineGeo); // non - collidable
 
 		float radius = 2.00f; //end.getDimension() / 2;
-        //ball = new Sphere(32, 32, radius);
-        ball = new Dome(Vector3f.ZERO, 2, 32, radius, false); // Cone
-        ballGeo = new Geometry("ball", ball);
-        ballGeo.setMaterial(game.getMagentaGlowMaterial());
-        
-        game.addNonCollidable(ballGeo);
+        cone = new Dome(Vector3f.ZERO, 2, 32, radius, false); // Cone
+        coneGeo = new Geometry("cone", cone);
+        coneGeo.setMaterial(game.getMagentaGlowMaterial());
+        game.addNonCollidable(coneGeo);
+
+        disk = new Cylinder(32, 32, radius, 0.10f, true); // Base of cone
+        diskGeo = new Geometry("disk", disk);
+        diskGeo.setMaterial(game.getMagentaGlowMaterial());
+        game.addNonCollidable(diskGeo);
         
         computePositions();
 
@@ -89,22 +89,26 @@ public class Edge implements ChangeListener {
 		line.updatePoints(startVec, endVec);
 
         Vector3f startToEnd = endVec.subtract(startVec);
-        int endDimension = end.getDimension();
-        // not updating ball size, building a new sphere damages performance.
         
-        Vector3f ballPos = startVec.add(startToEnd.divide(2.00f)); //startVec.add(startToEnd.normalize().mult(endDimension));
-        ballGeo.setLocalTranslation(ballPos);
+        Vector3f conePos = startVec.add(startToEnd.divide(2.00f)); //startVec.add(startToEnd.normalize().mult(endDimension));
+        coneGeo.setLocalTranslation(conePos);
 
-        /* This quaternion stores a rolling rotation */
+        // This quaternion stores the cones rotation to align with reference
         Quaternion rotation = new Quaternion();
-        rotation.fromAngleAxis( FastMath.PI / 2 , startToEnd.cross(new Vector3f(0, 1, 0)) );
-        ballGeo.setLocalRotation(rotation);
-        ballGeo.setLocalScale(1.00f, 3.00f, 1.00f);
+        rotation.fromAngleAxis(FastMath.PI / 2, startToEnd.cross(new Vector3f(0, 1, 0)));
+        coneGeo.setLocalRotation(rotation);
+        coneGeo.setLocalScale(1.00f, 3.00f, 1.00f);
+
+        // TODO: Needs changing to rotate disk into correct position
+        rotation.fromAngleAxis(FastMath.PI / 2, startToEnd.cross(new Vector3f(0, 1, 0)));
+        diskGeo.setLocalTranslation(conePos);
+        diskGeo.setLocalRotation(rotation);
     }
 
     void removeFromWorld(Game game) {
         game.removeNonCollidable(lineGeo);
-        game.removeNonCollidable(ballGeo);
+        game.removeNonCollidable(coneGeo);
+        game.removeNonCollidable(diskGeo);
     }
 
 }
